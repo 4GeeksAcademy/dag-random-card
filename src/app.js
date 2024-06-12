f7edb05c4b13a487f9068ea187fb689904e60796
dalex1981a <168557868+dalex1981a@users.noreply.github.com>
2024-06-12 22:50:09 +0000

    completed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,65 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./style.css";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  // document.querySelector("#number") = generateRandomNumber();
+
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+
+  let suit = generateRandomSuit();
+
+  document.querySelector(".topSuit").innerHTML = suit;
+
+  document.querySelector(".bottomSuit").innerHTML = suit;
+
+  if (suit === "&hearts;" || suit === "&diams;") {
+    document.querySelector(".topSuit").style.color = "red";
+
+    document.querySelector(".bottomSuit").style.color = "red";
+  }
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+
+    "2",
+
+    "3",
+
+    "4",
+
+    "5",
+
+    "6",
+
+    "7",
+
+    "8",
+
+    "9",
+
+    "10",
+
+    "J",
+
+    "Q",
+
+    "K"
+  ];
+
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suits = ["&hearts;", "&clubs;", "&diams;", "&spades;"];
+
+  let indexSuits = Math.floor(Math.random() * suits.length);
+
+  return suits[indexSuits];
 };
